2708e1449b1471048fc6d6a27b0ce22893f06db4
Ryan Ellis <ryan.ellis@uwaterloo.ca>
2026-08-18 11:29:12 -0400

    some bugfixes

--- src/main/scala/tilelink/CacheCork.scala
+++ src/main/scala/tilelink/CacheCork.scala
@@ -188,6 +188,8 @@ class TLCacheCork(params: TLCacheCorkParams = TLCacheCorkParams())(implicit p: P
         c_deq := false.B
         a_enq := false.B
         a_deq := false.B
+        val c_block = Wire(Bool())
+        c_block := false.B
 
         // high-priority queue is a bigger queue with no fancy things going on.
         val a_enq_ptr = Counter(params.a_queue_depth)
@@ -228,7 +230,7 @@ class TLCacheCork(params: TLCacheCorkParams = TLCacheCorkParams())(implicit p: P
         // ready signals
         c_a.ready := (!(c_full || a_deq || enq_c_req_to_a)) || c_a_to_a_queue.fire
         out_a_q.ready := !a_full
-        a_a.valid := in.a.valid && !toD && !c_deq
+        a_a.valid := in.a.valid && !toD && !c_block
 
         // handle multi-beat nonsense and tagmatching
         val bandwidth_ctr = RegInit(0.U(8.W))
@@ -247,7 +249,7 @@ class TLCacheCork(params: TLCacheCorkParams = TLCacheCorkParams())(implicit p: P
         val tagmatch_ptr = tagmatch_ptr_part + Mux(beats_left_tgmtch === 0.U, 0.U, beats_init_tgmtch - beats_left_tgmtch + 1.U)
         val tagmatch_latch = RegInit(false.B)
 
-        in.a.ready := Mux(toD, a_d.ready && !(tagmatch_valid || tagmatch_latch), (a_a.ready && !c_deq))
+        in.a.ready := Mux(toD, a_d.ready && !(tagmatch_valid || tagmatch_latch), (a_a.ready && !c_block))
 
         val addr_old = RegInit(chiselTypeOf(a_a.bits.address), 0.U)
 
@@ -326,13 +328,18 @@ class TLCacheCork(params: TLCacheCorkParams = TLCacheCorkParams())(implicit p: P
                 a_deq := true.B
                 c_deq_ptr.inc()
                 c_deq := true.B 
+                c_block := true.B 
                 when (tagmatch_valid) {
                   c_q_mem(tagmatch_ptr) := a_q_mem(a_deq_ptr.value)
                 }.otherwise {
                   c_q_mem(c_enq_ptr.value) := a_q_mem(a_deq_ptr.value)
                   c_enq_ptr.inc()
                   c_enq := true.B
-                  c_addr_map(c_deq_ptr.value).valid := false.B
+                  when (beats_left_tx === 1.U) { // update address pointer in table on last beat, point to first beat
+                      // THIS IS NOT SAFE FOR NON-POW2 SIZED WRITE BUFFERS.
+                      c_addr_map(c_enq_ptr.value - beats_init_tx).bits := a_q_mem(a_deq_ptr.value).address
+                      c_addr_map(c_enq_ptr.value - beats_init_tx).valid := true.B
+                  }
                 }
                 // ack write
                 when(addr_old =/= a_q_mem(a_deq_ptr.value).address) { // only for first beat
@@ -346,11 +353,12 @@ class TLCacheCork(params: TLCacheCorkParams = TLCacheCorkParams())(implicit p: P
               }
             }
           }.elsewhen (!c_empty) {
-            c_deq := true.B
+            c_block := true.B
             when (out.a.ready) {
               a_latency_bits := c_q_mem(c_deq_ptr.value)
               a_latency_valid := true.B 
               c_deq_ptr.inc()
+              c_deq := true.B
               c_addr_map(c_deq_ptr.value).valid := false.B
             }
           }
